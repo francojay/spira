0fb95f12f63a44a53b5ea6e0c123f7cecc5168ce
Machina <machina@Machinas-MacBook-Pro.local>
2018-10-17 17:44:04 -0500

    webreq

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Collections;
 using System;
 using System.Text.RegularExpressions;
@@ -12,54 +13,35 @@ public class Login : MonoBehaviour {
 	private String[] Lines;
 	private string DecryptedPass;
 
-	public void LoginButton(){
-		bool UN = false;
-		bool PW = false;
-        Debug.LogWarning("username", username);
-        Debug.LogWarning("password", password);
-
-        username.GetComponent<InputField>().text = "";
-        password.GetComponent<InputField>().text = "";
-        print("Login Sucessful");
-        Application.LoadLevel("Start Menu");
-        //if (Username != ""){
-        //	if(System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt")){
-        //		UN = true;
-        //		Lines = System.IO.File.ReadAllLines(@"E:/UnityTestFolder/"+Username+".txt");
-        //	} else {
-        //		Debug.LogWarning("Username Invaild");
-        //	}
-        //} else {
-        //	Debug.LogWarning("Username Field Empty");
-        //}
-        //if (Password != ""){
-        //	if (System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt")){
-        //		int i = 1;
-        //		foreach(char c in Lines[2]){
-        //			i++;
-        //			char Decrypted = (char)(c / i);
-        //			DecryptedPass += Decrypted.ToString();
-        //		}
-        //		if (Password == DecryptedPass){
-        //			PW = true;
-        //		} else {
-        //			Debug.LogWarning("Password Is invalid");
-        //		}
-        //	} else {
-        //		Debug.LogWarning("Password Is invalid");
-        //	}
-        //} else {
-        //	Debug.LogWarning("Password Field Empty");
-        //}
-        //      if (UN == true&&PW == true){
-        //	username.GetComponent<InputField>().text = "";
-        //	password.GetComponent<InputField>().text = "";	
-        //	print ("Login Sucessful");
-        //	Application.LoadLevel("Start Menu");
-        //}
+    public void LoginButton()
+    {
+        StartCoroutine(AuthUser());
     }
-	// Update is called once per frame
-	void Update () {
+
+    IEnumerator AuthUser()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/api/v1/users"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+                Application.LoadLevel("Start Menu");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if (Input.GetKeyDown(KeyCode.Tab)){
 			if (username.GetComponent<InputField>().isFocused){
 				password.GetComponent<InputField>().Select();
